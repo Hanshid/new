32bf54257b12663266a58173d4394198296b3603
hanshid <pahanshid@gmail.com>
2021-10-19 20:20:25 +0530

    new

---
+++ hanshidapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+def testfun(request):
+    return render(request,'hansh.html')
+def testfun(request):
+    return render(request,'hello.html')
+# Create your views here.
